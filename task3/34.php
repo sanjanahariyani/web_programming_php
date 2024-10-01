38fe1947b4728eaa6333144c3c3160f4997238f0
sanjanahariyani <mcasanjana05@gmail.com>
2024-10-01 13:09:42 +0530

    Add files via upload

---
+++ task3/34.php
@@ -0,0 +1,12 @@
+<?php
+$people = array("sanju", "deep", "kruti", "mahek");
+
+if (in_array("diya", $people))
+  {
+  echo "Match found";
+  }
+else
+  {
+  echo "Match not found";
+  }
+?>
---
+++ task3/35.php
@@ -0,0 +1,5 @@
+<?php
+$my_array=array("red","blue","green","yellow","black");
+shuffle($my_array);
+print_r($my_array);
+?>
